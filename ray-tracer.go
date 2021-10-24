cc9ef6c35755be8585b7a3d6cdadf3d75e0a05e8
Shamanskiy <shamanskiy.alexander@gmail.com>
2021-10-24 20:11:09 +0100

    refactor

--- ray-tracer.go
+++ ray-tracer.go
@@ -22,11 +22,15 @@ func main() {
 		for y := 0; y < height; y++ {
 			r := float64(x) / float64(width)
 			g := float64(y) / float64(height)
-			img.Set(x, y, color.RGBA{uint8(math.Round(255.99 * r)), uint8(math.Round(255.99 * g)), uint8(math.Round(255.99 * 0.2)), 0xff})
+			img.Set(x, y, color.RGBA{toZero255(r), toZero255(g), toZero255(0.2), 0xff})
 		}
 	}
 
 	// Encode as PNG.
 	f, _ := os.Create("image.png")
 	png.Encode(f, img)
+}
+
+func toZero255(x float64) uint8 {
+	return uint8(math.Round(255.99 * x))
 }
