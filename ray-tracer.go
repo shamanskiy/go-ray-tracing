f0043ebd236cd3d7b6894930064f7ac36ea35298
Shamanskiy <shamanskiy.alexander@gmail.com>
2021-10-24 20:05:07 +0100

    print first image

--- ray-tracer.go
+++ ray-tracer.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math"
+	"os"
+)
 
 func main() {
-	fmt.Println("Let's trace some rays!")
+	width := 200
+	height := 100
+
+	upLeft := image.Point{0, 0}
+	lowRight := image.Point{width, height}
+
+	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+
+	// Set color for each pixel.
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			r := float64(x) / float64(width)
+			g := float64(y) / float64(height)
+			img.Set(x, y, color.RGBA{uint8(math.Round(255.99 * r)), uint8(math.Round(255.99 * g)), uint8(math.Round(255.99 * 0.2)), 0xff})
+		}
+	}
+
+	// Encode as PNG.
+	f, _ := os.Create("image.png")
+	png.Encode(f, img)
 }
