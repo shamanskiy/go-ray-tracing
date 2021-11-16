57811388be447c8619d356d4e8aeb8a9dd5dc8e1
Shamanskiy <shamanskiy.alexander@gmail.com>
2021-11-16 20:08:21 +0000

    improve scene testing

--- render/scene_test.go
+++ render/scene_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/Shamanskiy/go-ray-tracer/core"
+	"github.com/Shamanskiy/go-ray-tracer/materials"
+	"github.com/Shamanskiy/go-ray-tracer/objects"
 )
 
 func TestScene_Default(t *testing.T) {
@@ -69,44 +71,78 @@ func TestScene_Empty(t *testing.T) {
 
 }
 
-// test scene.hitClosestObject
-// test scene.testRay with white skybox and single sphere
-// test multiple reflections (how?)
-
-/*func TestScene_RightSphere(t *testing.T) {
+func TestScene_HitClosetObject(t *testing.T) {
 	leftSphere := objects.Sphere{core.Vec3{-6.0, 0.0, 0.0}, 2.0}
 	rightSphere := objects.Sphere{core.Vec3{0.0, 0.0, 0.0}, 2.0}
+	material := materials.Diffusive{core.Black}
 	t.Logf("Given a scene with two spheres %v and %v,\n", leftSphere, rightSphere)
-	var scene Scene
-	scene.Add(leftSphere, rightSphere)
+	scene := Scene{}
+	scene.Add(leftSphere, material)
+	scene.Add(rightSphere, material)
 
 	hitRay := core.Ray{core.Vec3{4.0, 0.0, 0.0}, core.Vec3{-1.0, 0.0, 0.0}}
-	t.Logf("\twe can test if a ray with origin %v and direction %v hits anything:\n", hitRay.Origin, hitRay.Direction)
-	hitRecord := scene.Hit(hitRay)
-	expected := objects.HitRecord{Param: 2.0, Point: core.Vec3{2.0, 0.0, 0.0}, Normal: core.Vec3{1.0, 0.0, 0.0}}
-	if *hitRecord == expected {
-		t.Logf("\t\tPASSED: the ray hit the right sphere, hit record is %v, expected %v", hitRecord, expected)
+	t.Logf("\ta ray with origin %v and direction %v should hit the right sphere:\n", hitRay.Origin, hitRay.Direction)
+	hitRecord, objectIndex := scene.hitClosestObject(hitRay, 0.001)
+
+	expectedHit := objects.HitRecord{Param: 2.0, Point: core.Vec3{2.0, 0.0, 0.0}, Normal: core.Vec3{1.0, 0.0, 0.0}}
+	if *hitRecord == expectedHit {
+		t.Logf("\t\tPASSED: hit record is %v, expected %v", hitRecord, expectedHit)
 	} else {
-		t.Fatalf("\t\tFAILED: hit record is %v, expected %v", hitRecord, expected)
+		t.Fatalf("\t\tFAILED: hit record is %v, expected %v", hitRecord, expectedHit)
 	}
+	expectedIndex := 1
+	if objectIndex == expectedIndex {
+		t.Logf("\t\tPASSED: object index is %v, expected %v", objectIndex, expectedIndex)
+	} else {
+		t.Fatalf("\t\tFAILED: object index is %v, expected %v", objectIndex, expectedIndex)
+	}
+
+	t.Logf("\ta ray with origin %v, direction %v and minimum parameter 7.0 should hit the left sphere:\n", hitRay.Origin, hitRay.Direction)
+	hitRecord, objectIndex = scene.hitClosestObject(hitRay, 7.0)
+
+	expectedHit = objects.HitRecord{Param: 8.0, Point: core.Vec3{-4.0, 0.0, 0.0}, Normal: core.Vec3{1.0, 0.0, 0.0}}
+	if *hitRecord == expectedHit {
+		t.Logf("\t\tPASSED: hit record is %v, expected %v", hitRecord, expectedHit)
+	} else {
+		t.Fatalf("\t\tFAILED: hit record is %v, expected %v", hitRecord, expectedHit)
+	}
+	expectedIndex = 0
+	if objectIndex == expectedIndex {
+		t.Logf("\t\tPASSED: object index is %v, expected %v", objectIndex, expectedIndex)
+	} else {
+		t.Fatalf("\t\tFAILED: object index is %v, expected %v", objectIndex, expectedIndex)
+	}
+
 }
 
-func TestScene_LeftSphere(t *testing.T) {
-	leftSphere := objects.Sphere{core.Vec3{-6.0, 0.0, 0.0}, 2.0}
-	rightSphere := objects.Sphere{core.Vec3{0.0, 0.0, 0.0}, 2.0}
-	t.Logf("Given a scene with two spheres %v and %v,\n", leftSphere, rightSphere)
-	var scene Scene
-	scene.Add(leftSphere, rightSphere)
+func TestScene_TestRay_SingleSphere(t *testing.T) {
+	t.Log("Given a scene with a blue sphere and a white skybox,")
+	skyColor := core.White
+	scene := Scene{SkyColorBottom: skyColor, SkyColorTop: skyColor}
+	sphere := objects.Sphere{core.Vec3{0.0, 0.0, 0.0}, 1.0}
+	sphereColor := core.Blue
+	material := materials.Diffusive{sphereColor}
+	scene.Add(sphere, material)
 
 	hitRay := core.Ray{core.Vec3{4.0, 0.0, 0.0}, core.Vec3{-1.0, 0.0, 0.0}}
-	t.Logf("\twe can test if a ray with origin %v and direction %v hits anything with minimum parameter 7.0:\n",
-		hitRay.Origin, hitRay.Direction)
-	hitRecord := scene.HitWithMin(hitRay, 7.0)
-	expected := objects.HitRecord{Param: 8.0, Point: core.Vec3{-4.0, 0.0, 0.0}, Normal: core.Vec3{1.0, 0.0, 0.0}}
-	if *hitRecord == expected {
-		t.Logf("\t\tPASSED: the ray hit the left sphere, hit record is %v, expected %v", hitRecord, expected)
+	t.Logf("\ta ray with origin %v and direction %v should return blue color:\n", hitRay.Origin, hitRay.Direction)
+	rayColor := scene.TestRay(hitRay)
+
+	expectedColor := sphereColor
+	if rayColor == expectedColor {
+		t.Logf("\t\tPASSED: ray color is %v, expected %v", rayColor, expectedColor)
 	} else {
-		t.Fatalf("\t\tFAILED: hit record is %v, expected %v", hitRecord, expected)
+		t.Fatalf("\t\tFAILED: ray color is %v, expected %v", rayColor, expectedColor)
+	}
+
+	hitRay = core.Ray{core.Vec3{4.0, 0.0, 0.0}, core.Vec3{0.0, 1.0, 0.0}}
+	t.Logf("\ta ray with origin %v and direction %v should return white color:\n", hitRay.Origin, hitRay.Direction)
+	rayColor = scene.TestRay(hitRay)
+
+	expectedColor = skyColor
+	if rayColor == expectedColor {
+		t.Logf("\t\tPASSED: ray color is %v, expected %v", rayColor, expectedColor)
+	} else {
+		t.Fatalf("\t\tFAILED: ray color is %v, expected %v", rayColor, expectedColor)
 	}
 }
-*/
