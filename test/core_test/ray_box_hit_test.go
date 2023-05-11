f7e71ca54525949e812ef3a490ea1516b8d6baed
shamanskiy <shamanskiy.alexander@gmail.com>
2023-05-11 13:38:41 +0200

    add more box ray hit tests

--- test/core_test/ray_box_hit_test.go
+++ test/core_test/ray_box_hit_test.go
@@ -27,3 +27,46 @@ func TestRayShouldNotHitBox_IfRayPointsInWrongDirection(t *testing.T) {
 
 	assert.False(t, ray.Hits(box, 0, 10))
 }
+
+func TestRayShouldHitBox_IfRayParallelToBoxSide(t *testing.T) {
+	ray := core.NewRay(core.NewVec3(0, 0, 0), core.NewVec3(1, 0, 0))
+	box := core.NewBox(core.NewVec3(1, -1, -1), core.NewVec3(3, 1, 1))
+
+	assert.True(t, ray.Hits(box, 0, 10))
+}
+
+func TestRayShouldHitBox_IfRayTravelsInNegativeDirection(t *testing.T) {
+	ray := core.NewRay(core.NewVec3(5, 0, 0), core.NewVec3(-1, 0, 0))
+	box := core.NewBox(core.NewVec3(1, -1, -1), core.NewVec3(3, 1, 1))
+
+	assert.True(t, ray.Hits(box, 0, 10))
+}
+
+func TestRayShouldHitBox_IfRayOriginIsOnBoxBoundary(t *testing.T) {
+	ray := core.NewRay(core.NewVec3(1, 0, 0), core.NewVec3(1, 0, 0))
+	box := core.NewBox(core.NewVec3(1, -1, -1), core.NewVec3(3, 1, 1))
+
+	assert.True(t, ray.Hits(box, 0, 10))
+}
+
+func TestRayShouldHitBox_IfRayGoesThroughBoxSide(t *testing.T) {
+	ray := core.NewRay(core.NewVec3(0, 1, 0), core.NewVec3(1, 0, 0))
+	box := core.NewBox(core.NewVec3(1, -1, -1), core.NewVec3(3, 1, 1))
+
+	assert.True(t, ray.Hits(box, 0, 10))
+}
+
+func TestRayShouldHitBox_IfRayGoesThroughBoxEdge(t *testing.T) {
+	ray := core.NewRay(core.NewVec3(0, 1, 1), core.NewVec3(1, 0, 0))
+	box := core.NewBox(core.NewVec3(1, -1, -1), core.NewVec3(3, 1, 1))
+
+	assert.True(t, ray.Hits(box, 0, 10))
+}
+
+// Why ray-box hit is detected for sides and edges but not for corners? No idea
+func TestRayShouldNotHitBox_IfRayGoesThroughBoxCorner(t *testing.T) {
+	ray := core.NewRay(core.NewVec3(0, 0, 0), core.NewVec3(1, -1, -1))
+	box := core.NewBox(core.NewVec3(1, -1, -1), core.NewVec3(3, 1, 1))
+
+	assert.False(t, ray.Hits(box, 0, 10))
+}
