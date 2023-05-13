1edd8a38dcc7105ed058470aff2df7bd263c2c9c
shamanskiy <shamanskiy.alexander@gmail.com>
2023-05-13 17:04:57 +0200

    add failing test for triangle-ray test

--- test/scene_test/geometries_test/triangle_test.go
+++ test/scene_test/geometries_test/triangle_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Shamanskiy/go-ray-tracer/src/core"
 	"github.com/Shamanskiy/go-ray-tracer/src/scene/geometries"
+	"github.com/chewxy/math32"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -30,4 +31,28 @@ func TestTriangle_RayShouldMiss(t *testing.T) {
 	hit := triangle.TestRay(ray, core.NewInterval(0, 10))
 
 	assert.True(t, hit.Empty())
+}
+
+func TestTriangle_ShouldReturnNoHitBecauseOutsideOfParamInterval(t *testing.T) {
+	triangle := geometries.NewTriangle(
+		core.NewVec3(1, 0, 0),
+		core.NewVec3(0, 1, 0),
+		core.NewVec3(0, 0, 1))
+	ray := core.NewRay(core.NewVec3(0, 0, 0), core.NewVec3(1, 1, 1))
+
+	hit := triangle.TestRay(ray, core.NewInterval(0, 0.1))
+
+	assert.True(t, hit.Empty())
+}
+
+func TestTriangle_ShouldReturnHit(t *testing.T) {
+	triangle := geometries.NewTriangle(
+		core.NewVec3(1, 0, 0),
+		core.NewVec3(0, 1, 0),
+		core.NewVec3(0, 0, 1))
+	ray := core.NewRay(core.NewVec3(0, 0, 0), core.NewVec3(1, 1, 1))
+
+	hit := triangle.TestRay(ray, core.NewInterval(0, 10))
+
+	assert.Equal(t, math32.Sqrt(2), hit.Value().Param)
 }
